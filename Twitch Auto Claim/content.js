fb3107860798be77a008535ba112ca9d33f820ff
Starlight_Harmony <71433392+BambooCat0509@users.noreply.github.com>
2025-06-30 19:52:09 +0800

    Update content.js

--- Twitch Auto Claim/content.js
+++ Twitch Auto Claim/content.js
@@ -1,13 +1,15 @@
-function claimLoyaltyPoints() {
-	const rewardContainer = document.querySelector('div.ScTransitionBase-sc-hx4quq-0.OBUFH.tw-transition');
-	if (rewardContainer && rewardContainer.getAttribute('aria-hidden') === 'false') {
-		const claimButton = document.querySelector('button[aria-label="領取額外獎勵"].ScCoreButton-sc-ocjdkq-0.hpBkMI');
-		if (claimButton) {
-			var time = new Date();
-			console.log(time.toLocaleString(), ": 成功領取忠誠點數");
-			claimButton.click();
-			chrome.runtime.sendMessage({ type: "log", text: "成功領取忠誠點數" });
-		}
+chrome.runtime.onInstalled.addListener(() => {
+	console.log("Twitch 忠誠點數自動領取擴充插件已安裝");
+});
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (message.type === "log") {
+		const logMessage = `[${new Date().toLocaleString()}] ${message.text}\n`;
+		console.log("[Twitch Autoclaim]", message.text);
+		chrome.storage.local.get({ logs: "" }, (data) => {
+			const updatedLogs = data.logs + logMessage;
+			chrome.storage.local.set({ logs: updatedLogs }, () => {
+				chrome.runtime.sendMessage({ type: "log_response", text: message.text });
+			});
+		});
 	}
-}
-setInterval(claimLoyaltyPoints, 5000);
+});
